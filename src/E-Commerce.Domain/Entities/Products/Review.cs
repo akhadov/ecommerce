5caf1f0bab918eac58426981de8921319626b74b
akhadov <shakhriyor.akhadov@gmail.com>
2022-10-03 15:34:16 +0500

    first

---
+++ src/E-Commerce.Domain/Entities/Products/Review.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.Domain.Commons;
+using E_Commerce.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.Products
+{
+    public class Review : Auditable
+    {
+        public decimal Rating { get; set; }
+        public string Comment { get; set; } = String.Empty;
+
+        public long ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public long UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ src/E-Commerce.Domain/Entities/Users/User.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Commons;
+using E_Commerce.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.Users
+{
+    public class User : Auditable
+    {
+        public string Name { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+
+        public UserRole UserRole { get; set; }
+
+    }
+}
