b8b878d3cadd9a23065626b1bb830554d570267c
akhadov <shakhriyor.akhadov@gmail.com>
2022-10-05 13:48:26 +0500

    IRepositories

---
+++ src/E-Commerce.Data/IRepositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using E_Commerce.Domain.Entities.Orders;
+
+namespace E_Commerce.Data.IRepositories
+{
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+    }
+}
---
+++ src/E-Commerce.Data/IRepositories/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using E_Commerce.Domain.Entities.Products;
+
+namespace E_Commerce.Data.IRepositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
